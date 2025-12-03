88a13ad686cf3f0ada365ff57a1b3944a2d11825
junx806 <junxi806@gmail.com>
2025-12-03 17:03:18 +0800

    header + first 7 fn

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: gjun-xi <gjun-xi@student.42singapore.sg>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 21:18:04 by gjun-xi           #+#    #+#             */
-/*   Updated: 2025/12/01 21:19:43 by gjun-xi          ###   ########.fr       */
+/*   Updated: 2025/12/03 16:53:23 by gjun-xi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,12 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
 
 #endif
