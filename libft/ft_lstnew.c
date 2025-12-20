a52b704e915e81dd388fdebeb37148ab403121b3
Gan Jun Xi <gjun-xi@c2r2s14.42singapore.sg>
2025-12-20 22:26:38 +0800

    started on lstnew, yet to debug

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -6,8 +6,20 @@
 /*   By: gjun-xi <gjun-xi@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/20 20:03:21 by gjun-xi           #+#    #+#             */
-/*   Updated: 2025/12/20 20:03:22 by gjun-xi          ###   ########.fr       */
+/*   Updated: 2025/12/20 22:24:54 by gjun-xi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+t_list *ft_lstnew(void *content){
+t_list *res;
+
+res = (t_list *)malloc(sizeof(t_list));
+if (!res){
+return NULL;
+}
+res -> content = content;
+res -> next = NULL;
+return (res);
+}
